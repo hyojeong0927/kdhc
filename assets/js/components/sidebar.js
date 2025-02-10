c4f3506f8069bab73ef50cb0fc79c11951712bf3
hyojeong0927 <hyojeong0927@gmail.com>
2025-02-10 20:13:44 +0900

    top nav active 추가가

--- assets/js/components/sidebar.js
+++ assets/js/components/sidebar.js
@@ -1,16 +1,38 @@
 document.getElementById('toggleButton').addEventListener('click', function() {
     var sidebar = document.getElementById('sidebar');
-    var menu = document.getElementById('menu');
     var menuIcon = document.getElementById('menuIcon');
 
     sidebar.classList.toggle('collapsed');
-    // menu.classList.toggle('collapse');
-    
+
     if (sidebar.classList.contains('collapsed')) {
-      menuIcon.classList.remove('arrow-close');
-      menuIcon.classList.add('arrow-expansion');
+        menuIcon.classList.remove('arrow-close');
+        menuIcon.classList.add('arrow-expansion');
     } else {
-      menuIcon.classList.remove('arrow-expansion');
-      menuIcon.classList.add('arrow-close');
+        menuIcon.classList.remove('arrow-expansion');
+        menuIcon.classList.add('arrow-close');
     }
+});
+
+document.querySelectorAll('.lnb-item').forEach(function(item) {
+    item.addEventListener('click', function() {
+        document.querySelectorAll('.lnb-item').forEach(function(otherItem) {
+            otherItem.classList.remove('active');
+        });
+        this.classList.add('active');
+    });
+});
+
+// top nav
+document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach(link => {
+        link.addEventListener("click", function () {
+            // 모든 nav-link에서 active 클래스 제거
+            navLinks.forEach(item => item.classList.remove("active"));
+
+            // 현재 클릭된 nav-link에 active 클래스 추가
+            this.classList.add("active");
+        });
+    });
 });
