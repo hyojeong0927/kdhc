1d62ce5c1cb1c2dfae18886e75a12e2522fec5ae
Hyojeong Kim <hyokim0927@gmail.com>
2025-02-13 10:56:04 +0900

    작업리스트 수정정

--- html/worklist/js/data/index.js
+++ html/worklist/js/data/index.js
@@ -1,4 +1,4 @@
-import components from './components.js';
+// import components from './components.js';
 import menu00Projects from './menu00.js';
 import menu01Projects from './menu01.js';
 import menu02Projects from './menu02.js';
@@ -9,7 +9,7 @@ import menu06Projects from './menu06.js';
 import menu07Projects from './menu07.js';
 
 const allProjects = [
-    ...components,
+    // ...components,
     ...menu00Projects,
     ...menu01Projects,
     ...menu02Projects,
