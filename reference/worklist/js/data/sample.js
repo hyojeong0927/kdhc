4e06fe883337f3fb8cba995d45d5b838a8a9226b
hyojeong0927 <hyojeong0927@gmail.com>
2025-02-22 17:18:45 +0900

    샘플페이지 추가

---
+++ reference/worklist/js/data/sample.js
@@ -0,0 +1,18 @@
+const sample = [
+    {
+        id: "common",
+        depth1: "sample",
+        depth2: "",
+        depth3: "",
+        type: "",
+        folder: "sample",
+        file: "sample-01.html",
+        status: "",
+        start: "",
+        end: "",
+        modDate: "",
+        design: "",
+        dev: "",
+    },
+];
+export default sample;
