5b1bd1375ea76f5dcbd1ab29b3adb51548fe3068
hyojeong0927 <hyojeong0927@gmail.com>
2025-03-04 20:01:51 +0900

    체크 옵션 추가

--- assets/js/components/checkbox.js
+++ assets/js/components/checkbox.js
@@ -13,21 +13,21 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-    const weekSelection = document.querySelector(".col2 .form-group .form-check-group"); // 점검요일 영역
-    const radioButtons = document.querySelectorAll("input[name='drone1']"); // 점검 주기 유형
+// document.addEventListener("DOMContentLoaded", function () {
+//     const weekSelection = document.querySelector(".col2 .form-group .form-check-group"); // 점검요일 영역
+//     const radioButtons = document.querySelectorAll("input[name='drone1']"); // 점검 주기 유형
 
-    // 초기 숨김 처리
-    weekSelection.closest(".col2").style.display = "none";
+//     // 초기 숨김 처리
+//     weekSelection.closest(".col2").style.display = "none";
 
-    // 이벤트 리스너 추가
-    radioButtons.forEach((radio) => {
-        radio.addEventListener("change", function () {
-            if (this.value === "radio02") {
-                weekSelection.closest(".col2").style.display = "block"; // 주간 선택 시 표시
-            } else {
-                weekSelection.closest(".col2").style.display = "none"; // 다른 옵션 선택 시 숨김
-            }
-        });
-    });
-});
+//     // 이벤트 리스너 추가
+//     radioButtons.forEach((radio) => {
+//         radio.addEventListener("change", function () {
+//             if (this.value === "radio02") {
+//                 weekSelection.closest(".col2").style.display = "block"; // 주간 선택 시 표시
+//             } else {
+//                 weekSelection.closest(".col2").style.display = "none"; // 다른 옵션 선택 시 숨김
+//             }
+//         });
+//     });
+// });
