61e276f5ffaabdad21f84afbae52af44e29f7ed2
Hyojeong Kim <hyokim0927@gmail.com>
2025-03-12 14:30:25 +0900

    메인 dashboard 작업

--- html/home/chart/chart.js
+++ html/home/chart/chart.js
@@ -1,5 +1,6 @@
 const { AgCharts } = agCharts;
 
+// 저장매체 보유현황
 const options1 = {
     container: document.getElementById("statusChart"),
     title: {
@@ -49,7 +50,9 @@ const options1 = {
                 fontFamily: 'Noto Sans KR',
                 color: '#A5A5A5',
                 rotation: -45
-            }
+            },
+            paddingInner: 0.6,
+            paddingOuter: 0,
         },
         {
             type: "number",
@@ -90,6 +93,7 @@ const options1 = {
         }
     },
 };
+// 공개취약점 자산현황
 const options2 = {
     container: document.getElementById("assetsChart"),
     title: {
@@ -123,7 +127,9 @@ const options2 = {
                 fontFamily: 'Noto Sans KR',
                 color: '#A5A5A5',
                 rotation: -45
-            }
+            },
+            paddingInner: 0.6,
+            paddingOuter: 0,
         },
         {
             type: "number",
@@ -145,51 +151,313 @@ const options2 = {
     ],
     legend: {
         enabled: false,
-        position: 'top',
-        spacing: 20,
-        item: {
-            paddingX: 12,
-            paddingY: 0,
-            marker: {
-                shape: 'circle',
-                size: 8,
-                padding: 5,
+    },
+};
+// 백신 업데이트 현황
+const options3 = {
+    container: document.getElementById("vaccineChart"),
+    title: {
+        text: "",
+    },
+    subtitle: {
+        text: "",
+    },
+    data: getVaccineData(),
+    series: [
+        {
+            type: "bar",
+            xKey: "quarter",
+            yKey: "vaccine",
+            yName: "vaccine",
+            fill: "#D774F2",
+            stacked: true,
+        },
+    ],
+    padding: { top: 14, right: 0, bottom: 0, left: 0 },
+    axes: [
+        {
+            type: "category",
+            position: "bottom",
+            gridLine: {
+                enabled: false,
             },
             label: {
-                color: '#18191C',
+                fontSize: 10,
+                fontWeight: 'normal',
+                fontFamily: 'Noto Sans KR',
+                color: '#A5A5A5',
+                rotation: -45
+            },
+            paddingInner: 0.6,
+            paddingOuter: 0,
+        },
+        {
+            type: "number",
+            position: "left",
+            interval: {
+                step: 10
+            },
+            gridLine: {
+                enabled: true,
+            },
+            label: {
                 fontSize: 12,
                 fontWeight: 'normal',
                 fontFamily: 'Noto Sans KR',
+                color: '#A5A5A5',
+                rotation: 0
             }
-        }
+        },
+    ],
+    legend: {
+        enabled: false,
     },
 };
+// 전사 자산현항
+const options4 = {
+    container: document.getElementById("propertyChart"),
+    title: {
+        text: "",
+    },
+    subtitle: {
+        text: "",
+    },
+    data: getPropertyData(),
+    series: [
+        {
+            type: "bar",
+            xKey: "quarter",
+            yKey: "property",
+            yName: "property",
+            fill: "#3FB3E4",
+            stacked: true,
+        },
+    ],
+    padding: { top: 14, right: 0, bottom: 0, left: 0 },
+    axes: [
+        {
+            type: "category",
+            position: "bottom",
+            gridLine: {
+                enabled: false,
+            },
+            label: {
+                fontSize: 10,
+                fontWeight: 'normal',
+                fontFamily: 'Noto Sans KR',
+                color: '#A5A5A5',
+                rotation: -45
+            },
+            paddingInner: 0.6,
+            paddingOuter: 0,
+        },
+        {
+            type: "number",
+            position: "left",
+            interval: {
+                step: 10
+            },
+            gridLine: {
+                enabled: true,
+            },
+            label: {
+                fontSize: 12,
+                fontWeight: 'normal',
+                fontFamily: 'Noto Sans KR',
+                color: '#A5A5A5',
+                rotation: 0
+            }
+        },
+    ],
+    legend: {
+        enabled: false,
+    },
+};
+// TEST
+const options5 = {
+    container: document.getElementById("testChart"),
+    title: {
+        text: "",
+    },
+    subtitle: {
+        text: "",
+    },
+    data: getTestData(),
+    series: [
+        {
+            type: "bar",
+            xKey: "quarter",
+            yKey: "test",
+            yName: "test",
+            fill: "#EFD125",
+            stacked: true,
+        },
+    ],
+    padding: { top: 14, right: 0, bottom: 0, left: 0 },
+    axes: [
+        {
+            type: "category",
+            position: "bottom",
+            gridLine: {
+                enabled: false,
+            },
+            label: {
+                fontSize: 10,
+                fontWeight: 'normal',
+                fontFamily: 'Noto Sans KR',
+                color: '#A5A5A5',
+                rotation: -45
+            },
+            paddingInner: 0.6,
+            paddingOuter: 0,
+        },
+        {
+            type: "number",
+            position: "left",
+            interval: {
+                step: 10
+            },
+            gridLine: {
+                enabled: true,
+            },
+            label: {
+                fontSize: 12,
+                fontWeight: 'normal',
+                fontFamily: 'Noto Sans KR',
+                color: '#A5A5A5',
+                rotation: 0
+            }
+        },
+    ],
+    legend: {
+        enabled: false,
+    },
+};
+// 지원종료운영체제자산현황
+const options6 = {
+    container: document.getElementById("systemChart"),
+    title: {
+        text: "",
+    },
+    subtitle: {
+        text: "",
+    },
+    data: getSystemData(),
+    series: [
+        {
+            type: "bar",
+            xKey: "quarter",
+            yKey: "system",
+            yName: "system",
+            fill: "#3BBC5D",
+            stacked: true,
+        },
+    ],
+    padding: { top: 14, right: 0, bottom: 0, left: 0 },
+    axes: [
+        {
+            type: "category",
+            position: "bottom",
+            gridLine: {
+                enabled: false,
+            },
+            label: {
+                fontSize: 10,
+                fontWeight: 'normal',
+                fontFamily: 'Noto Sans KR',
+                color: '#A5A5A5',
+                rotation: -45
+            },
+            paddingInner: 0.6,
+            paddingOuter: 0,
+        },
+        {
+            type: "number",
+            position: "left",
+            interval: {
+                step: 10
+            },
+            gridLine: {
+                enabled: true,
+            },
+            label: {
+                fontSize: 12,
+                fontWeight: 'normal',
+                fontFamily: 'Noto Sans KR',
+                color: '#A5A5A5',
+                rotation: 0
+            }
+        },
+    ],
+    legend: {
+        enabled: false,
+    },
+};
+// 정기점검 (주간/월간)
+const options7 = {
+    container: document.getElementById("checkChart"),
+    title: {
+        text: "",
+    },
+    subtitle: {
+        text: "",
+    },
+    data: getCheckData(),
+    series: [
+        {
+            type: "bar",
+            xKey: "quarter",
+            yKey: "check",
+            yName: "check",
+            fill: "#9FA1EE",
+            stacked: true,
+        },
+    ],
+    padding: { top: 14, right: 0, bottom: 0, left: 0 },
+    axes: [
+        {
+            type: "category",
+            position: "bottom",
+            gridLine: {
+                enabled: false,
+            },
+            label: {
+                fontSize: 10,
+                fontWeight: 'normal',
+                fontFamily: 'Noto Sans KR',
+                color: '#A5A5A5',
+                rotation: -45
+            },
+            paddingInner: 0.6,
+            paddingOuter: 0,
+        },
+        {
+            type: "number",
+            position: "left",
+            interval: {
+                step: 10
+            },
+            gridLine: {
+                enabled: true,
+            },
+            label: {
+                fontSize: 12,
+                fontWeight: 'normal',
+                fontFamily: 'Noto Sans KR',
+                color: '#A5A5A5',
+                rotation: 0
+            }
+        },
+    ],
+    legend: {
+        enabled: false,
+    },
+};
+
 const chart1 = AgCharts.create(options1);
 const chart2 = AgCharts.create(options2);
-
-function updateLegendPosition(value) {
-    options.legend.position = value;
-    chart1.update(options1);
-    chart2.update(options2);
-}
-
-function setLegendEnabled(enabled) {
-    options1.legend.enabled = enabled;
-    options1.legend.enabled = enabled;
-    chart1.update(options1);
-    chart2.update(options2);
-}
-
-function setStep(step) {
-    const axis = options.axes?.[1];
-    axis.interval = { step: step };
-    chart1.update(options);
-    chart2.update(options);
-}
-
-function resetInterval() {
-    const axis = options.axes?.[1];
-    axis.interval = {};
-    chart1.update(options1);
-    chart2.update(options2);
-}
+const chart3 = AgCharts.create(options3);
+const chart4 = AgCharts.create(options4);
+const chart5 = AgCharts.create(options5);
+const chart6 = AgCharts.create(options6);
+const chart7 = AgCharts.create(options7);
--- html/home/chart/data.js
+++ html/home/chart/data.js
@@ -1,3 +1,4 @@
+// 저장매체보유현황
 function getStatusData() {
     return [
         {
@@ -116,7 +117,7 @@ function getStatusData() {
         },
     ];
 };
-
+// 공개취약점 자산현황
 function getAssetsData() {
     return [
         {
@@ -194,6 +195,411 @@ function getAssetsData() {
         {
             quarter: "평택지사",
             assets: 22,
+        },
+    ];
+}
+// 백신 업데이트 현황
+function getVaccineData() {
+    return [
+        {
+            quarter: "화성지사",
+            vaccine: 20,
+        },
+        {
+            quarter: "파주지사",
+            vaccine: 45,
+        },
+        {
+            quarter: "동탄지사",
+            vaccine: 35,
+        },
+        {
+            quarter: "고양사업소",
+            vaccine: 25,
+        },
+        {
+            quarter: "용인지사",
+            vaccine: 15,
+        },
+        {
+            quarter: "강남지사",
+            vaccine: 25,
+        },
+        {
+            quarter: "광교지사",
+            vaccine: 5,
+        },
+        {
+            quarter: "광주전남지사",
+            vaccine: 22,
+        },
+        {
+            quarter: "김해사업소",
+            vaccine: 20,
+        },
+        {
+            quarter: "대구지사",
+            vaccine: 25,
+        },
+        {
+            quarter: "분당사업소",
+            vaccine: 33,
+        },
+        {
+            quarter: "삼송지사",
+            vaccine: 6,
+        },
+        {
+            quarter: "세종지사",
+            vaccine: 44,
+        },
+        {
+            quarter: "수원사업소",
+            vaccine: 15,
+        },
+        {
+            quarter: "양산지사",
+            vaccine: 25,
+        },
+        {
+            quarter: "중앙지사",
+            vaccine: 8,
+        },
+        {
+            quarter: "청주지사",
+            vaccine: 14,
+        },
+        {
+            quarter: "판교지사",
+            vaccine: 6,
+        },
+        {
+            quarter: "평택지사",
+            vaccine: 22,
+        },
+    ];
+}
+// 전사 자산현항
+function getPropertyData() {
+    return [
+        {
+            quarter: "화성지사",
+            property: 20,
+        },
+        {
+            quarter: "파주지사",
+            property: 45,
+        },
+        {
+            quarter: "동탄지사",
+            property: 35,
+        },
+        {
+            quarter: "고양사업소",
+            property: 25,
+        },
+        {
+            quarter: "용인지사",
+            property: 15,
+        },
+        {
+            quarter: "강남지사",
+            property: 25,
+        },
+        {
+            quarter: "광교지사",
+            property: 5,
+        },
+        {
+            quarter: "광주전남지사",
+            property: 22,
+        },
+        {
+            quarter: "김해사업소",
+            property: 20,
+        },
+        {
+            quarter: "대구지사",
+            property: 25,
+        },
+        {
+            quarter: "분당사업소",
+            property: 33,
+        },
+        {
+            quarter: "삼송지사",
+            property: 6,
+        },
+        {
+            quarter: "세종지사",
+            property: 44,
+        },
+        {
+            quarter: "수원사업소",
+            property: 15,
+        },
+        {
+            quarter: "양산지사",
+            property: 25,
+        },
+        {
+            quarter: "중앙지사",
+            property: 8,
+        },
+        {
+            quarter: "청주지사",
+            property: 14,
+        },
+        {
+            quarter: "판교지사",
+            property: 6,
+        },
+        {
+            quarter: "평택지사",
+            property: 22,
+        },
+    ];
+}
+// TEST
+function getTestData() {
+    return [
+        {
+            quarter: "화성지사",
+            test: 20,
+        },
+        {
+            quarter: "파주지사",
+            test: 45,
+        },
+        {
+            quarter: "동탄지사",
+            test: 35,
+        },
+        {
+            quarter: "고양사업소",
+            test: 25,
+        },
+        {
+            quarter: "용인지사",
+            test: 15,
+        },
+        {
+            quarter: "강남지사",
+            test: 25,
+        },
+        {
+            quarter: "광교지사",
+            test: 5,
+        },
+        {
+            quarter: "광주전남지사",
+            test: 22,
+        },
+        {
+            quarter: "김해사업소",
+            test: 20,
+        },
+        {
+            quarter: "대구지사",
+            test: 25,
+        },
+        {
+            quarter: "분당사업소",
+            test: 33,
+        },
+        {
+            quarter: "삼송지사",
+            test: 6,
+        },
+        {
+            quarter: "세종지사",
+            test: 44,
+        },
+        {
+            quarter: "수원사업소",
+            test: 15,
+        },
+        {
+            quarter: "양산지사",
+            test: 25,
+        },
+        {
+            quarter: "중앙지사",
+            test: 8,
+        },
+        {
+            quarter: "청주지사",
+            test: 14,
+        },
+        {
+            quarter: "판교지사",
+            test: 6,
+        },
+        {
+            quarter: "평택지사",
+            test: 22,
+        },
+    ];
+}
+// 지원종료운영체제자산현황
+function getSystemData() {
+    return [
+        {
+            quarter: "화성지사",
+            system: 20,
+        },
+        {
+            quarter: "파주지사",
+            system: 45,
+        },
+        {
+            quarter: "동탄지사",
+            system: 35,
+        },
+        {
+            quarter: "고양사업소",
+            system: 25,
+        },
+        {
+            quarter: "용인지사",
+            system: 15,
+        },
+        {
+            quarter: "강남지사",
+            system: 25,
+        },
+        {
+            quarter: "광교지사",
+            system: 5,
+        },
+        {
+            quarter: "광주전남지사",
+            system: 22,
+        },
+        {
+            quarter: "김해사업소",
+            system: 20,
+        },
+        {
+            quarter: "대구지사",
+            system: 25,
+        },
+        {
+            quarter: "분당사업소",
+            system: 33,
+        },
+        {
+            quarter: "삼송지사",
+            system: 6,
+        },
+        {
+            quarter: "세종지사",
+            system: 44,
+        },
+        {
+            quarter: "수원사업소",
+            system: 15,
+        },
+        {
+            quarter: "양산지사",
+            system: 25,
+        },
+        {
+            quarter: "중앙지사",
+            system: 8,
+        },
+        {
+            quarter: "청주지사",
+            system: 14,
+        },
+        {
+            quarter: "판교지사",
+            system: 6,
+        },
+        {
+            quarter: "평택지사",
+            system: 22,
+        },
+    ];
+}
+// 정기점검 (주간/월간)
+function getCheckData() {
+    return [
+        {
+            quarter: "화성지사",
+            check: 20,
+        },
+        {
+            quarter: "파주지사",
+            check: 45,
+        },
+        {
+            quarter: "동탄지사",
+            check: 35,
+        },
+        {
+            quarter: "고양사업소",
+            check: 25,
+        },
+        {
+            quarter: "용인지사",
+            check: 15,
+        },
+        {
+            quarter: "강남지사",
+            check: 25,
+        },
+        {
+            quarter: "광교지사",
+            check: 5,
+        },
+        {
+            quarter: "광주전남지사",
+            check: 22,
+        },
+        {
+            quarter: "김해사업소",
+            check: 20,
+        },
+        {
+            quarter: "대구지사",
+            check: 25,
+        },
+        {
+            quarter: "분당사업소",
+            check: 33,
+        },
+        {
+            quarter: "삼송지사",
+            check: 6,
+        },
+        {
+            quarter: "세종지사",
+            check: 44,
+        },
+        {
+            quarter: "수원사업소",
+            check: 15,
+        },
+        {
+            quarter: "양산지사",
+            check: 25,
+        },
+        {
+            quarter: "중앙지사",
+            check: 8,
+        },
+        {
+            quarter: "청주지사",
+            check: 14,
+        },
+        {
+            quarter: "판교지사",
+            check: 6,
+        },
+        {
+            quarter: "평택지사",
+            check: 22,
         },
     ];
 }
