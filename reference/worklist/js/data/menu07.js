5ec0fafaba30a246d9a62dc7e749c4409c72f002
Hyojeong Kim <hyokim0927@gmail.com>
2025-03-19 15:06:07 +0900

    가이드 정리

--- reference/worklist/js/data/menu07.js
+++ reference/worklist/js/data/menu07.js
@@ -100,7 +100,7 @@ const menu07Projects = [
         status: "완료",
         start: "2025-03-04",
         end: "2025-03-07",
-        modDate: "",
+        modDate: "2025-03-19",
         design: "",
         dev: "2025-03-24",
     },
