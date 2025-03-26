0644174b335905eba97626b2adfa73c525a869d9
Hyojeong Kim <hyokim0927@gmail.com>
2025-03-26 13:49:14 +0900

    주석 수정

--- assets/js/grid/indexGrid.js
+++ assets/js/grid/indexGrid.js
@@ -106,7 +106,7 @@ const indexColumnDefs = [
                         <input type="search" class="form-control" id="search1" placeholder="이름" value="">
                         <label class="input-group-text" for="search1"><i class="icon search"></i></label>
                     </div>
-                    <!--// input search group end -->
+                    <!--// input search group -->
                 `;
             } else {
                 return value;
